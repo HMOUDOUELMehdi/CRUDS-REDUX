ad489241f9e1d3830c4957054d390b33a2d7276a
HmoudouElMehdi <elmehdihmoudou0@gmail.com>
2024-01-13 23:09:11 +0100

    nothing

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,26 @@
 import {
   createBrowserRouter,
   RouterProvider,
+  // useNavigate,
 } from "react-router-dom";
 import Register from "./Login && Rogister/Register";
 import Login from "./Login && Rogister/Login";
 import ListTasks from "./Tasks/ListTasks";
 import Task from "./Tasks/Task";
+// import { useEffect } from "react";
 
 
 function App() {
+
+  // const navigate = useNavigate()
+
+  // const PrivateRoot = ({ element }) => {
+  //   const logInSuccess = validLogin
+  //   useEffect(() => {
+  //     console.log("isAuthenticated: " + isAuthenticated);
+  //   }, [isAuthenticated]);
+  //   return logInSuccess ? element : navigate("/login")
+  // }
 
   const router = createBrowserRouter([
     {
@@ -22,6 +34,7 @@ function App() {
     },    
     {
       path: "/task",
+      // element: <PrivateRoot element={ <Task /> } /> ,
       element: <Task /> ,
       // element: <ListTasks /> ,
     },   
