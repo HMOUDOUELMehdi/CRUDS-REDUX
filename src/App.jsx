a35771846c12e6350a5e5e2d43c0e0d63e639669
HmoudouElMehdi <elmehdihmoudou0@gmail.com>
2024-01-21 00:08:35 +0100

    private root

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,37 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-  useNavigate,
-} from "react-router-dom";
+import React from "react";
+import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
 import Register from "./Login && Rogister/Register";
 import Login from "./Login && Rogister/Login";
-// import ListTasks from "./Tasks/ListTasks";
 import Task from "./Tasks/Task";
-import { useEffect } from "react";
 import { useSelector } from 'react-redux';
 
-
 function App() {
-
-  const navigate = useNavigate()
-
-  const PrivateRoot = ({ element }) => {
+  function PrivateRoute({ element }) {
     const logInSuccess = useSelector((state) => state.login.loginSuccess);
 
-    useEffect(() => {
-      console.log("isAuthenticated: " + isAuthenticated);
-    }, [isAuthenticated]);
-    return logInSuccess ? element : navigate("/login")
+    return logInSuccess ? element : <Navigate to="/login" replace />;
   }
 
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Register /> ,
+      element: <Register />,
     },
     {
       path: "/login",
-      element: <Login /> ,
-    },    
+      element: <Login />,
+    },
     {
       path: "/task",
-      element: <PrivateRoot element={ <Task /> } /> ,
-      // element: <Task /> ,
-      // element: <ListTasks /> ,
-    } 
+      element: <PrivateRoute element={<Task />} />,
+    },
   ]);
 
   return (
     <>
-    <RouterProvider router={router} />
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
