97aeb33d7b5808d69860312babaa92b0bcb2966b
HmoudouElMehdi <elmehdihmoudou0@gmail.com>
2024-01-20 23:55:18 +0100

    plus

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,28 @@
 import {
   createBrowserRouter,
   RouterProvider,
-  // useNavigate,
+  useNavigate,
 } from "react-router-dom";
 import Register from "./Login && Rogister/Register";
 import Login from "./Login && Rogister/Login";
-import ListTasks from "./Tasks/ListTasks";
+// import ListTasks from "./Tasks/ListTasks";
 import Task from "./Tasks/Task";
-// import { useEffect } from "react";
+import { useEffect } from "react";
+import { useSelector } from 'react-redux';
 
 
 function App() {
 
-  // const navigate = useNavigate()
+  const navigate = useNavigate()
 
-  // const PrivateRoot = ({ element }) => {
-  //   const logInSuccess = validLogin
-  //   useEffect(() => {
-  //     console.log("isAuthenticated: " + isAuthenticated);
-  //   }, [isAuthenticated]);
-  //   return logInSuccess ? element : navigate("/login")
-  // }
+  const PrivateRoot = ({ element }) => {
+    const logInSuccess = useSelector((state) => state.login.loginSuccess);
+
+    useEffect(() => {
+      console.log("isAuthenticated: " + isAuthenticated);
+    }, [isAuthenticated]);
+    return logInSuccess ? element : navigate("/login")
+  }
 
   const router = createBrowserRouter([
     {
@@ -33,15 +35,10 @@ function App() {
     },    
     {
       path: "/task",
-      // element: <PrivateRoot element={ <Task /> } /> ,
-      element: <Task /> ,
+      element: <PrivateRoot element={ <Task /> } /> ,
+      // element: <Task /> ,
       // element: <ListTasks /> ,
-    },   
-    {
-      path: "/task1",
-      element: <ListTasks /> ,
-      // element: <ListTasks /> ,
-    },    
+    } 
   ]);
 
   return (
--- src/StoreDetails/Store.jsx
+++
@@ -1,4 +0,0 @@
-// import { legacy_createStore } from "redux";
-// import RootReducer from "./RootReducer";
-
-// const Store = legacy_createStore(RootReducer)
